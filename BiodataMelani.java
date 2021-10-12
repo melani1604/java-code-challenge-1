3b9fb1b3019730351284b17b00715de86521ce82
melani1604 <melanimischa1604@gmail.com>
2021-10-12 17:35:58 +0700

    first commit

---
+++ BiodataMelani.java
@@ -0,0 +1,35 @@
+public class BiodataMelani {
+    public static void main(String[] args){
+        String firstname,lastname,address,hobby,favoritefood;
+        int age;
+        long phone;
+        double height;
+        char blood;
+        Boolean single;
+        
+
+        firstname    = "Melani";
+        lastname     = "Misca";
+        address      ="Batam";
+        age          = 21;
+        phone        = +628953795;
+        height       = 155;
+        blood        = 'O';
+        single       = true;
+        hobby        = "Main Piano";
+        favoritefood = "Bakso";
+        
+        
+        System.out.println("Name            : " + firstname + lastname + "\n" +
+                        "Address         : " + address + "\n" +
+      					"Age             : " + age + " Tahun" + "\n" +
+                        "Phone           : " + phone + "\n" +
+                        "Height          : " + height + " cm"+"\n" +
+                        "Blood           : " + blood + "\n" +
+                        "Single          : " + single + "\n" +
+                        "Hobby           : " + hobby + "\n" +
+                        "Favorite Food   : " + favoritefood + "\n\n" + "Selesai");
+
+
+    }
+}
